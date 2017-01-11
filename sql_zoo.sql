9348f3722d0c310125d5afdd6d999f3824b7cd2e
Westw00d <westerhout_tom@hotmail.com>
2017-01-11 21:43:47 +0100

    Added SELECT basics and names

---
+++ sql_zoo.sql
@@ -0,0 +1,93 @@
+# SELECT basics
+
+# 1.
+SELECT population FROM world
+WHERE name = 'Germany';
+
+# 2
+SELECT name, population 
+FROM world
+WHERE name IN ('Ireland', 'Iceland', 'Denmark');
+
+# 3
+SELECT name, area
+FROM world
+WHERE area BETWEEN 200000 AND 250000;
+
+# SELECT names
+
+# 1
+SELECT name 
+FROM world
+WHERE name LIKE 'Y%';
+
+# 2
+SELECT name 
+FROM world
+WHERE name LIKE '%y';
+
+# 3
+SELECT name 
+FROM world
+WHERE name LIKE '%x%';
+
+# 4
+SELECT name 
+FROM world
+WHERE name LIKE '%land';
+
+# 5
+SELECT name 
+FROM world
+WHERE name LIKE 'C%ia';
+
+# 6
+SELECT name 
+FROM world
+WHERE name LIKE '%oo%';
+
+# 7
+SELECT name 
+FROM world
+WHERE name LIKE '%a%a%a%';
+
+# 8
+SELECT name 
+FROM world
+WHERE name LIKE '_t%'
+ORDER BY name;
+
+# 9
+SELECT name 
+FROM world
+WHERE name LIKE '%o__o%';
+
+# 10
+SELECT name 
+FROM world
+WHERE name LIKE '____';
+
+# 11
+SELECT name
+FROM world
+WHERE name = capital;
+
+# 12
+SELECT name
+FROM world
+WHERE capital LIKE '%City';
+
+# 13
+SELECT capital, name
+FROM world
+WHERE capital LIKE concat(name, '%');
+
+# 14
+SELECT name, capital
+FROM world
+WHERE capital LIKE CONCAT(name, '_%');
+
+# 15
+SELECT name, REPLACE(capital, name, '')
+FROM world
+WHERE capital LIKE CONCAT(name, '_%');
