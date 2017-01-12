2c357431c2580cd0fe7e2ee0e5f0c074f4fa67c4
Westw00d <westerhout_tom@hotmail.com>
2017-01-12 14:40:31 +0100

    Added select from world

--- select_names.sql
+++ select_names.sql
@@ -1,19 +1,3 @@
-# SELECT basics
-
-# 1.
-SELECT population FROM world
-WHERE name = 'Germany';
-
-# 2
-SELECT name, population 
-FROM world
-WHERE name IN ('Ireland', 'Iceland', 'Denmark');
-
-# 3
-SELECT name, area
-FROM world
-WHERE area BETWEEN 200000 AND 250000;
-
 # SELECT names
 
 # 1
