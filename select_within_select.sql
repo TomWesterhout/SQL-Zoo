ac17b103ab0f0bf15b8a5d68816c3b91bd81ad1b
Westw00d <westerhout_tom@hotmail.com>
2017-01-13 10:44:47 +0100

    Added select within select

---
+++ select_within_select.sql
@@ -0,0 +1,25 @@
+# SELECT within SELECT
+
+#1
+SELECT name
+FROM world
+WHERE population > (SELECT population 
+					FROM world 
+					WHERE name = 'Russia');
+
+#2
+SELECT name
+FROM world
+WHERE continent = 'Europe' 
+AND gdp/population > (SELECT gdp/population 
+						FROM world 
+						WHERE name = 'United Kingdom');
+
+#3
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent 
+					FROM world 
+					WHERE name 
+					IN ('Argentina', 'Australia'))
+ORDER BY name;
