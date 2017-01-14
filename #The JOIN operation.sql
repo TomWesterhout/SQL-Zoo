f918a6e08498df60f843db59ad6597d1663a6509
Westw00d <westerhout_tom@hotmail.com>
2017-01-14 17:42:16 +0100

    Updated the join operation file

--- #The JOIN operation.sql
+++ #The JOIN operation.sql
@@ -1,3 +1,28 @@
 #The JOIN operation
 
 #1
+SELECT matchid, player
+FROM goal 
+WHERE teamid = 'GER';
+
+#2
+SELECT id, stadium, team1, team2
+FROM game
+WHERE id = 1012;
+
+#3
+SELECT player, teamid, stadium, mdate
+FROM goal JOIN game ON (matchid=id)
+WHERE teamid = 'GER';
+
+#4
+SELECT team1, team2, player
+FROM game JOIN goal ON (id=matchid)
+WHERE player LIKE 'Mario%';
+
+#5
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam ON (teamid=id)
+WHERE gtime <= 10;
+
+#6
