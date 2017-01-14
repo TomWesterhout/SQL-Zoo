526182f649c37edb8c09cb8bb2f17a490e866ebf
Westw00d <westerhout_tom@hotmail.com>
2017-01-14 14:24:26 +0100

    Added sum and count

---
+++ sum_and_count.sql
@@ -0,0 +1,31 @@
+#SUM and COUNT
+
+#1
+SELECT SUM(population)
+FROM world;
+
+#2
+SELECT DISTINCT(continent)
+FROM world;
+
+#3
+SELECT SUM(gdp)
+FROM world
+WHERE continent = 'Africa';
+
+#4
+SELECT COUNT(name)
+FROM world
+WHERE area >= 1000000;
+
+#5
+SELECT SUM(population)
+FROM world
+WHERE name IN ('France', 'Germany', 'Spain');
+
+#6
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent;
+
+#7
