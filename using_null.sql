3cd8f7da1e10420329cf2681ddf775795d8d6edd
Westw00d <westerhout_tom@hotmail.com>
2017-01-16 19:10:11 +0100

    Updated using null file

--- using_null.sql
+++ using_null.sql
@@ -10,3 +10,33 @@ SELECT teacher.name, dept.name
 FROM teacher 
 INNER JOIN dept
 ON (teacher.dept=dept.id);
+
+#3
+SELECT teacher.name, dept.name
+FROM teacher
+LEFT JOIN dept
+ON teacher.dept = dept.id;
+
+#4
+SELECT teacher.name, dept.name
+FROM teacher
+RIGHT JOIN dept
+ON teacher.dept = dept.id;
+
+#5
+SELECT name, 
+COALESCE(mobile, '07986 444 2266') AS number
+FROM teacher;
+
+#6
+SELECT teacher.name,
+COALESCE(dept.name, 'None')
+FROM teacher
+LEFT JOIN dept
+ON teacher.dept = dept.id;
+
+#7
+SELECT COUNT(name) AS name, COUNT(mobile) AS number
+FROM teacher;
+
+#8
